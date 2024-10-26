cc780ca2da6446857a95d8d771306ddc51d20319
Haknozer <hkanoer@gmail.com>
2024-10-26 15:57:19 +0300

    High Score Add

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import random
-
+import time
+import json
+import os
 def main():
     rndNumber =  random.randint(1,100)
     chances = 0
     select = ""
     chancesCounter = 0
     correct = False
+    record = []
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
     print("You have 5 chances to guess the correct number.")
@@ -16,6 +19,15 @@ def main():
     print("3. Hard (3 chances)")
 
     while True:
+        
+        if not os.path.exists("record.json"):
+            open("record.json","x")
+        try:
+            with open ("record.json","r") as json_file:
+                record = json.load(json_file)
+                
+        except:
+                record = []
         try:
             difficulty=int((input("Your choice : ")))
 
@@ -43,18 +55,23 @@ def main():
     print("Let's start the game!")
 
     while chancesCounter != chances:
+        start = time.time()
         chancesCounter += 1
         try:
             userGuess = int(input("Enter your guess: "))
         except ValueError:
             print("Only Number")
+            chancesCounter -= 1
 
         if userGuess > rndNumber:
             print(f"Incorrect! The number is less than {userGuess}.")
         elif userGuess < rndNumber:
             print(f"Incorrect! The number is greater than {userGuess}.")
         else:
-            print(f"Congratulations! You guessed the correct number in {chancesCounter} attempts.")
+            end = time.time()
+            length  = end - start
+            print(f"Congratulations! You guessed the correct number in {chancesCounter} attempts. You found the correct number in {length} seconds")
+            updateRecord(length,select,record)
             correct = True
             break
 
@@ -66,4 +83,15 @@ def main():
     if restart.upper() == "Y":
         print("das")
         main()
+
+def updateRecord(time,select,record):
+    if select in record:
+        for x in record:
+            if x == select and record[x] > time:
+                record[x] = time
+    else:
+        record[select] = time
+
+    with open("record.json","w") as json_file:
+        json.dump(record,json_file,indent=1)
 main()
